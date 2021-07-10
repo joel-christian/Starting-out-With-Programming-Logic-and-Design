69b0f8f00e207d93d0f09a14c083ae9cfca7aa8c
Joel Christian <86573791+joel-christian@users.noreply.github.com>
2021-07-10 16:58:55 -0500

    Update Kinetic Energy.py

--- Kinetic Energy.py
+++ Kinetic Energy.py
@@ -22,48 +22,35 @@
 
 def main():
 
-    #Get the mass of the object
-    m = getMass()
-
-    #Get the velocity of the object
-    v = getVelocity()
-
-    #Calculate the kinetic energy of the object
-    ke = calcKE(m, v)
+    ke = kineticEnergy()
 
     #Display the result
     print("\nThe kinetic energy of the object is: ", format(ke, '.2f'), "joules.")
 
-#The getMass module prompts the user to enter the mass of the object,
-#validates that the value is greater than 0,
-#and returns the value to the main module
-def getMass():
+#The kineticEnergy module prompts the user to enter the mass and velocity of the object,
+#validates that the input is greater than 0,
+#calculates the kinetic energy,
+#and sends it back to the main function.
+
+def kineticEnergy():
 
     #Prompt the user to input the mass of the object
-    mass = float(input("\nEnter the mass (kg) of the object: "))
+    m = float(input("\nEnter the mass (kg) of the object: "))
     
     #Validate that input is greater than 0
-    while mass <= 0:
-        print("ERROR: Mass must be greater than 0")
-        mass = float(input("\nEnterthe mass (kg) of the object: "))
-    return mass
-
-#The getVelocity module prompts the user to enter the velocity of the object,
-#validates that the value is greater than 0,
-#and returns the value to the main module
-def getVelocity():
+    while m <= 0:
+        print("ERROR: m must be greater than 0")
+        m = float(input("\nEnterthe mass (kg) of the object: "))
 
     #Prompt the user to input the velocity of the object
-    velocity = float(input("\nEnter the velocity (m/s) of the object: "))
+    v = float(input("\nEnter the velocity (m/s) of the object: "))
 
     #Validate that the input is greater than 0
-    while velocity <= 0:
-        print("ERROR: Velocity must be greater than 0")
-        velocity = float(input("\nEnter the velocity (m/s) of the object: "))
-    return velocity
-
-#The calcKE module calculates the kinetic energy of the object
-def calcKE(m, v):
+    while v <= 0:
+        print("ERROR: v must be greater than 0")
+        v = float(input("\nEnter the velocity (m/s) of the object: "))
+    
+    #Calculate the kinetic energy
     
     KE = 1/2 * m * v**2
     return KE
